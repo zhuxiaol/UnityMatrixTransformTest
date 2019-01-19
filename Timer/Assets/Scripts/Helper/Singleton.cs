812ecb3c11609e5ed90bfa48355b74c36fa90476
wangguoqing <540456313@qq.com>
2019-01-19 21:03:46 +0800

    Timer

---
+++ Timer/Assets/Scripts/Helper/Singleton.cs
@@ -0,0 +1,61 @@
+﻿//=====================================================
+// - FileName:      Singleton 
+// - Author:        codingriver
+// - Created:       2018/11/13 15:56:29
+// - CLR version:   4.0.30319.42000
+// - UserName:      Administrator
+//======================================================
+
+using System;
+using UnityEngine;
+
+namespace ETModel
+{
+    /// <summary>
+    /// 单例
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Singleton<T> :MonoBehaviour
+        where T : MonoBehaviour
+    {
+        private static T m_Instance;
+        private static object locker = new object();
+        public static T Instance
+        {
+            get
+            {
+                if (m_Instance == null)
+                {
+                    lock (locker)
+                    {
+                        if(m_Instance==null)
+                        {
+                            GameObject obj = new GameObject(typeof(T).Name);
+                            GameObject.DontDestroyOnLoad(obj);
+                            m_Instance = obj.AddComponent<T>();
+                        }
+                    }
+
+                }
+                return m_Instance;
+
+            }
+        }
+        public Singleton()
+        {
+            if (m_Instance != null)
+            {
+                throw (new Exception("Can't create singleton instance more than once."));
+            }
+        }
+
+        public void Dispose()
+        {
+            m_Instance = default(T);
+        }
+
+
+
+
+    }
+}
---
+++ Timer/Assets/Scripts/TimerTest.cs
@@ -0,0 +1,23 @@
+﻿//=====================================================
+// - FileName:      TimerTest.cs
+// - Created:       codingriver
+//======================================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerTest : MonoBehaviour {
+
+	// Use this for initialization
+	async void  Start () {
+        Debug.Log("Start---------------------------"+Time.realtimeSinceStartup);
+        await ETModel.TimerMgr.Instance.WaitAsync(1000 * 3);
+        Debug.Log("End---------------------------" + Time.realtimeSinceStartup);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
